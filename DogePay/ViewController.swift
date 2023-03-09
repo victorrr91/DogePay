94be38563d4a47dc50b02ab7a3aa278da867755e
victorrr91 <winhunu3@naver.com>
2023-03-09 16:35:27 +0900

    Initial Commit

---
+++ DogePay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogePay
+//
+//  Created by Victor Lee on 2023/03/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
